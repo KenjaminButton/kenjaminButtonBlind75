d20b4be36a20ec8215ef84975de67e547556f4fe
kendrickPC <kennethpchang@gmail.com>
2023-07-17 07:28:29 +0800

    Sign in button styled

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,41 @@
+import Link from 'next/link';
+import React from 'react';
+
+type NavbarProps = {
+  
+};
+
+const Navbar:React.FC<NavbarProps> = () => {
+  
+  return (
+    <div className='flex items-center justify-between sm:px-12 px-2 md:px-24'>
+      <Link href='/' className='flex items-center justify-center h-20'>
+        <img src='/logo.png' alt='Kenjamin Blind 75' className='h-full'/>
+      </Link>
+      <div className="flex items-center">
+        <button 
+          className='
+            bg-brand-orange 
+            text-white 
+            px-2 
+            py-1 
+            sm:px-4 
+            rounded-md 
+            text-sm 
+            font-medium
+            hover:text-brand-orange
+            hover:bg-white
+            hover:border-2
+            hover:border-brand-orange
+            border-2
+            border-transparent
+            transition
+            duration-300
+            ease-in-out
+            '
+        >Sign In</button>
+      </div>
+    </div>
+  )
+}
+export default Navbar;
---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,19 @@
+import Navbar from '@/components/Navbar/Navbar';
+import React from 'react';
+
+type AuthPageProps = {
+  
+};
+
+const AuthPage:React.FC<AuthPageProps> = () => {
+  
+  return (
+    // Background gradient will add later
+    <div className="">
+      <div className='max-w-7xl mx-auto'>
+        <Navbar />
+      </div>
+    </div>
+  )
+}
+export default AuthPage;
