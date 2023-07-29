eb702b28ad4b1f72921ef1bc3e0aabfc691c75bc
Kenneth P. Chang <kennethpchang@Kenneths-MacBook-Air.local>
2023-07-29 16:22:48 +0800

    Home page table and design of emoji land added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,42 @@
 import Topbar from "@/components/Topbar/Topbar";
 
 export default function Home() {
-  return (
-    <>
-      <main className="bg-dark-layer-2 min-h-screen">
-        <Topbar />
-      </main>
-    </>
-  )
+	return (
+		<>
+			<main className='bg-dark-layer-2 min-h-screen'>
+				<Topbar />
+				<h1
+					className='text-2xl text-center text-gray-700 dark:text-gray-400 font-medium
+					uppercase mt-10 mb-5'
+				>
+					&ldquo; Kenjamin's Blind 75 &rdquo; 
+          <p>😀 😃 😄 😁 😆 😅 😂 🤣 🥲 🥹 😊 😇 🙂 🙃 😉 😌 😍 🥰 😘 😗 😙 😚 😋 😛 😝 😜 🤪 🤨 🧐 🤓 😎 🥸 🤩 🥳 😏 😒 😞 😔 😟 😕 🙁 😣 😖 😫 😩 🥺 😢 😭 😮‍💨 😤 😠 😡 🤬 🤯 😳 🥵 🥶 😱 😨 😰 😥 😓 🫣 🤗 🫡 🤔 🫢 🤭 🤫 🤥 😶 😶‍🌫️ 😐 😑 😬 🫨 🫠 🙄 😯 😦 😧 😮 😲 🥱 😴 🤤 😪 😵 😵‍💫 🤐 🥴 🤢 🤮 🤧 😷 🤒 🤕 🤑 🤠 💩</p>
+				</h1>
+				<div className='relative overflow-x-auto mx-auto px-6 pb-10'>
+					<table className='text-sm text-left text-gray-500 dark:text-gray-400 sm:w-7/12 w-full max-w-[1200px] mx-auto'>
+						<thead className='text-xs text-gray-700 uppercase dark:text-gray-400 border-b '>
+							<tr>
+								<th scope='col' className='px-1 py-3 w-0 font-medium'>
+									Status
+								</th>
+								<th scope='col' className='px-6 py-3 w-0 font-medium'>
+									Title
+								</th>
+								<th scope='col' className='px-6 py-3 w-0 font-medium'>
+									Difficulty
+								</th>
+
+								<th scope='col' className='px-6 py-3 w-0 font-medium'>
+									Category
+								</th>
+								<th scope='col' className='px-6 py-3 w-0 font-medium'>
+									Solution
+								</th>
+							</tr>
+						</thead>
+					</table>
+				</div>
+			</main>
+		</>
+	);
 }
