4f5f4e8e622eaad8e055775e4cf939a3d1e8612f
KenjaminButton <kennethpchang@gmail.com>
2023-07-29 17:43:36 +0800

    All columns and rows rendered from mock data

--- src/components/ProblemsTable/ProblemsTable.tsx
+++ src/components/ProblemsTable/ProblemsTable.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { problems } from '@/mockData/problems';
 import {BsCheckCircle} from 'react-icons/bs'
+import {AiFillYoutube} from 'react-icons/ai'
 import Link from 'next/link';
 type ProblemsTableProps = {
     
@@ -11,6 +12,7 @@ const ProblemsTable:React.FC<ProblemsTableProps> = () => {
   return (
     <tbody className='text-white'>
       {problems.map( (doc, idx) => {
+        const difficultyColor = doc.difficulty === 'Easy' ? 'text-dark-green-s' : doc.difficulty === 'Medium' ? 'text-dark-yellow' : 'text-dark-pink'
         return (
           <tr className={`${idx % 2 === 1 ? `bg-dark-layer-1` : `` }`} key={doc.id}>
             <th className='px-2 py-4 font-medium whitespace-nowrap text-dark-green-s'>
@@ -21,8 +23,18 @@ const ProblemsTable:React.FC<ProblemsTableProps> = () => {
                 {doc.title}
               </Link>
             </td>
+            <td className={`px-6 py-4 ${difficultyColor}`}>
+              {doc.difficulty}
+            </td>
+            <td className={'px-6 py-4'}>
+              {doc.category}
+            </td>
+            <td className={'px-6 py-4'}>
+              {doc.videoId ? (<AiFillYoutube />) : (
+                <p className='text-gray-400'>I'll post this after I solve it</p>
+              )}
+            </td>
           </tr>
-          
         )
       })}
     </tbody>
