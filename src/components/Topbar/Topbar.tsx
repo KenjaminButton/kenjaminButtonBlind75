34c1b709b91516bce5cf7bfda5a08cc73242c8cb
KenjaminButton <kennethpchang@gmail.com>
2023-07-30 08:39:30 +0800

    Fixed getStaticPaths error by downgrading Next.js from 13.4.X to 13.2.4.

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,13 +1,16 @@
+import { auth } from '@/firebase/firebase';
 import Link from 'next/link';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 type TopbarProps = {
     
 };
 
 const Topbar:React.FC<TopbarProps> = () => {
-    
-    return (
+	const [user] = useAuthState(auth);
+
+  return (
 		<nav className='relative flex h-[50px] w-full shrink-0 items-center px-5 bg-indigo-400 text-white'>
 			<div className={`flex w-full items-center justify-between`}>
 				<Link href='/' className='h-[22px] flex-1'>
@@ -25,12 +28,13 @@ const Topbar:React.FC<TopbarProps> = () => {
 							Premium
 						</a>
 					</div>
-					
-                    <Link
-                        href='/auth'
-                    >
-                        <button className='bg-indigo-300 py-1 px-2 cursor-pointer rounded '>Sign In</button>
-                    </Link>
+
+				{!user && (
+					<Link href='/auth'>
+						<button className='bg-indigo-300 py-1 px-2 cursor-pointer rounded '>Sign In</button>
+					</Link>
+				)}
+
 				</div>
 			</div>
 		</nav>
