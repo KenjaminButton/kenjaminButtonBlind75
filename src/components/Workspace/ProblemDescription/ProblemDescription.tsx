e643be0cc7112517c6f9206395ace5b28840ea60
KenjaminButton <kennethpchang@gmail.com>
2023-08-03 07:35:08 +0800

    Like button logic implemented. Like button works, but runs VERY slow

--- src/components/Workspace/ProblemDescription/ProblemDescription.tsx
+++ src/components/Workspace/ProblemDescription/ProblemDescription.tsx
@@ -1,19 +1,79 @@
 import CircleSkeleton from '@/components/Skeletons/CircleSkeleton';
 import RectangleSkeleton from '@/components/Skeletons/RectangleSkeleton';
-import { firestore } from '@/firebase/firebase';
+import { auth, firestore } from '@/firebase/firebase';
 import { DBProblem, Problem } from '@/utils/types/problem';
-import { doc, getDoc } from 'firebase/firestore';
+import { doc, getDoc, runTransaction } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { AiFillLike, AiFillDislike } from "react-icons/ai";
 import { BsCheck2Circle } from "react-icons/bs";
 import { TiStarOutline } from "react-icons/ti";
+import { toast } from 'react-toastify';
 
 type ProblemDescriptionProps = {
   problem: Problem
 };
 
 const ProblemDescription: React.FC<ProblemDescriptionProps> = ({problem}) => {
-  const {currentProblem, loading, problemDifficultyClass} = useGetCurrentProblem(problem.id);
+  const [user] = useAuthState(auth)
+  const {currentProblem, loading, problemDifficultyClass, setCurrentProblem} = useGetCurrentProblem(problem.id);
+  const {liked, disliked, solved, setData, starred} = useGetUsersDataOnProblem(problem.id)
+  const [updating, setUpdating] = useState(false);
+
+  const handleLike = async () => {
+    if (!user) {
+      toast.error("You must be logged in to like a problem", {position: "top-left", theme: "dark"})
+      return
+    }
+    if (updating) return;
+		setUpdating(true);
+		await runTransaction(firestore, async (transaction) => {
+			const userRef = doc(firestore, "users", user.uid)
+      const problemRef = doc(firestore, "problems", problem.id)
+      const userDoc = await transaction.get(userRef)
+      const problemDoc = await transaction.get(problemRef)
+
+			if (userDoc.exists() && problemDoc.exists()) {
+				if (liked) {
+					// remove problem id from likedProblems on user document, decrement likes on problem document
+					transaction.update(userRef, {
+						likedProblems: userDoc.data().likedProblems.filter((id: string) => id !== problem.id),
+					});
+					transaction.update(problemRef, {
+						likes: problemDoc.data().likes - 1,
+					});
+
+					setCurrentProblem((prev) => (prev ? { ...prev, likes: prev.likes - 1 } : null));
+					setData((prev) => ({ ...prev, liked: false }));
+				} else if (disliked) {
+					transaction.update(userRef, {
+						likedProblems: [...userDoc.data().likedProblems, problem.id],
+						dislikedProblems: userDoc.data().dislikedProblems.filter((id: string) => id !== problem.id),
+					});
+					transaction.update(problemRef, {
+						likes: problemDoc.data().likes + 1,
+						dislikes: problemDoc.data().dislikes - 1,
+					});
+
+					setCurrentProblem((prev) =>
+						prev ? { ...prev, likes: prev.likes + 1, dislikes: prev.dislikes - 1 } : null
+					);
+					setData((prev) => ({ ...prev, liked: true, disliked: false }));
+				} else {
+					transaction.update(userRef, {
+						likedProblems: [...userDoc.data().likedProblems, problem.id],
+					});
+					transaction.update(problemRef, {
+						likes: problemDoc.data().likes + 1,
+					});
+					setCurrentProblem((prev) => (prev ? { ...prev, likes: prev.likes + 1 } : null));
+					setData((prev) => ({ ...prev, liked: true }));
+				}
+			}
+		});
+		setUpdating(false);
+  }
+
   return (
 		<div className='bg-dark-layer-1'>
 			{/* TAB */}
@@ -49,8 +109,27 @@ const ProblemDescription: React.FC<ProblemDescriptionProps> = ({problem}) => {
               <div className='rounded p-[3px] ml-4 text-lg transition-colors duration-200 text-green-s text-dark-green-s'>
                 <BsCheck2Circle />
               </div>
-              <div className='flex items-center cursor-pointer hover:bg-dark-fill-3 space-x-1 rounded p-[3px] ml-4 text-lg transition-colors duration-200 text-dark-gray-6'>
-                <AiFillLike />
+              <div 
+                className='
+                  flex 
+                  items-center 
+                  cursor-pointer 
+                  hover:bg-dark-fill-3 
+                  space-x-1 
+                  rounded 
+                  p-[3px] 
+                  ml-4 
+                  text-lg 
+                  transition-colors 
+                  duration-200 
+                  text-dark-gray-6                
+                '
+                onClick={handleLike}
+              >
+
+
+                {liked &&  <AiFillLike className='text-dark-blue-s'/>}
+                {!liked &&  <AiFillLike />}
                 <span className='text-xs'>{currentProblem.likes}</span>
               </div>
               <div className='flex items-center cursor-pointer hover:bg-dark-fill-3 space-x-1 rounded p-[3px] ml-4 text-lg transition-colors duration-200 text-dark-gray-6'>
@@ -150,18 +229,32 @@ function useGetCurrentProblem(problemId: string) {
     getCurrentProblem()
   }, [problemId])
 
-  return {currentProblem, loading, problemDifficultyClass}
+  return {currentProblem, loading, problemDifficultyClass, setCurrentProblem}
 }
 
-// Firebase Docs Get a Document
-// import { doc, getDoc } from "firebase/firestore";
+function useGetUsersDataOnProblem(problemId: string) {
+	const [data, setData] = useState({ liked: false, disliked: false, starred: false, solved: false });
+	const [user] = useAuthState(auth);
 
-// const docRef = doc(db, "cities", "SF");
-// const docSnap = await getDoc(docRef);
+	useEffect(() => {
+		const getUsersDataOnProblem = async () => {
+			const userRef = doc(firestore, "users", user!.uid);
+			const userSnap = await getDoc(userRef);
+			if (userSnap.exists()) {
+				const data = userSnap.data();
+				const { solvedProblems, likedProblems, dislikedProblems, starredProblems } = data;
+				setData({
+					liked: likedProblems.includes(problemId), // likedProblems["two-sum","jump-game"]
+					disliked: dislikedProblems.includes(problemId),
+					starred: starredProblems.includes(problemId),
+					solved: solvedProblems.includes(problemId),
+				});
+			}
+		};
 
-// if (docSnap.exists()) {
-//   console.log("Document data:", docSnap.data());
-// } else {
-//   // docSnap.data() will be undefined in this case
-//   console.log("No such document!");
-// }
+		if (user) getUsersDataOnProblem();
+		return () => setData({ liked: false, disliked: false, starred: false, solved: false });
+	}, [problemId, user]);
+
+	return { ...data, setData };
+}
