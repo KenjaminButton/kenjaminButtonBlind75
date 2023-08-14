b73c6bbeca1454a55b9802732304226d94aae714
KenjaminButton <kennethpchang@gmail.com>
2023-08-14 16:23:41 +0800

    &apos;

--- src/components/Modals/Login.tsx
+++ src/components/Modals/Login.tsx
@@ -50,7 +50,7 @@ const Login:React.FC<LoginProps> = () => {
 
   return (
     <form className="space y-6 px-6 py-4" onSubmit={handleLogin}>
-      <h3 className='text-xl font-medium text-white'>Sign into Kenjamins Blind 75</h3>
+      <h3 className='text-xl font-medium text-white'>Sign into Kenjamin&apos;s Blind 75</h3>
       <div>
         <label htmlFor='email' className='text-sm font-medium block mb-2 text-gray-300'>Your Email</label>
         <input 
